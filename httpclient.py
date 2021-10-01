4f48cc74ed236e1bb6b6595f2dcdea0bef5d2c77
danila seliayeu <dan.seliayeu@gmail.com>
2021-10-01 13:06:26 -0600

    make client fulfill user stories and pass all freetests.py tests

--- httpclient.py
+++ httpclient.py
@@ -22,7 +22,7 @@ import sys
 import socket
 import re
 # you may use urllib to encode data appropriately
-import urllib.parse
+from urllib.parse import urlparse, unquote, urlsplit
 
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
@@ -41,13 +41,40 @@ class HTTPClient(object):
         return None
 
     def get_code(self, data):
-        return None
+        status_line = data.split("\n")[0]
+        return status_line.split(" ")[1]
 
     def get_headers(self,data):
-        return None
+        return data.split("\r\n\r\n")[0]
+    
+    # generate an HTTP request with the given method, adding args to body
+    # in the case of a POST
+    def generate_request(self, method, args=None):
+        if method == "GET":
+            out = "GET " + str(self.path) + " HTTP/1.0\r\n"
+            out += "Host: " + str(self.url) + "\r\n"
+            out += "\r\n"
+            return out
+        elif method == "POST":
+            body = ""
+            # if there are args then format them as required by the Content-Type
+            if args:
+                for field, value in args.items():
+                    body += unquote(field) + "=" + unquote(value) + "&"
+            body = body[0:-1] if body else ""
+            out = "POST " + str(self.path) + " HTTP/1.0\r\n"
+            out += "Host: " + str(self.url) + "\r\n"
+            out += "Content-Type: application/x-www-form-urlencoded\r\n"
+            out += "Content-Length: " + str(len(body)) + "\r\n"
+            out += "\r\n"
+            out += body
+            out += "\r\n\r\n"
+            return out
+        else:
+            return None
 
     def get_body(self, data):
-        return None
+        return data.split("\r\n\r\n")[1]
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
@@ -67,15 +94,48 @@ class HTTPClient(object):
                 done = not part
         return buffer.decode('utf-8')
 
+    # call send_request with GET and do printing and set up object to return
     def GET(self, url, args=None):
-        code = 500
-        body = ""
+        print("\nGETting content at " + url + " with arguments " + str(args) + "...")
+        response = self.send_request(url, args, "GET")
+        code = int(self.get_code(response))
+        body = self.get_body(response)
+        print(response)
         return HTTPResponse(code, body)
 
+    # call send_request with POST and do printing and set up object to return
     def POST(self, url, args=None):
-        code = 500
-        body = ""
+        print("\nPOSTing to " + url + " with arguments " + str(args) + "...")
+        response = self.send_request(url, args, "POST")
+        code = int(self.get_code(response))
+        body = self.get_body(response)
+        print(response)
         return HTTPResponse(code, body)
+
+    # send the GET or POST request
+    def send_request(self, url, args, method):
+        self.parse_url(url)
+        self.connect(self.host, self.port)
+        self.sendall(self.generate_request(method, args))
+        response = self.recvall(self.socket)
+        self.close()
+        return response
+
+    # parse the specified url and save the various components as 
+    # objecet variables
+    def parse_url(self, url):
+        parsed = urlparse(unquote(url))
+        print(parsed)
+        self.url = parsed.netloc
+        self.path = parsed.path if parsed.path else "/"
+        split = self.url.split(":")
+        if len(split) == 2:
+            self.url = self.host = split[0]
+            self.port = int(split[1])
+        else:
+            self.host = socket.gethostbyname(self.url)
+            self.port = 80
+        return None
 
     def command(self, url, command="GET", args=None):
         if (command == "POST"):
